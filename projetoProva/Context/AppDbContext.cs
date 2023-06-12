cd15bc0b184a9c042afc7393e01ad4412701b4d0
sebastião <sebastiaojardimneto@yahoo.com>
2023-06-12 20:27:19 -0300

    first commit

---
+++ projetoProva/Context/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using projetoProva.model;
+
+namespace projetoProva.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>()
+                .Property(e => e.Nome)
+                .HasMaxLength(80);
+
+            modelBuilder.Entity<Produto>()
+                .Property(e => e.Valor)
+                .HasPrecision(10,2);
+
+            modelBuilder.Entity<Produto>()
+                .HasData(
+                new Produto { Id = 1, Nome = "Produto1", Valor = 1.89M, Quantidade = 10, Descricao = "Descricao", CodBarras = 512 },
+                new Produto { Id = 2, Nome = "Produto2", Valor = 2.76M, Quantidade = 5, Descricao = "Descricao", CodBarras = 215 }
+                );
+        }
+    }
+}
---
+++ projetoProva/model/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace projetoProva.model
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public int Quantidade { get; set; }
+        public string Descricao { get; set; }
+        public int CodBarras { get; set; }
+    }
+}
